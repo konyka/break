48581c147adff0e1c12445942e1a0bcccdc2ea4c
konyka <www.aion.us(at)gmail.com>
2023-10-06 16:13:14 +0800

    windows 平台，使用D3D 在窗口绘制填充颜色的三角形

---
+++ platform/windows/cbuffer.h
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * 
+ * 	break engine source file.
+ * 	copyright (C), break 
+ * 
+ * 	file name:   cbuffer.h
+ * 	version:     v1.0.0
+ * 	created:     2023/10/6 by SandBox
+ * 	compilers:   visual studio、clang、gcc
+ * 	description: 
+ * ---------------------------------------------------------------------------
+ * 	history: 
+ * 
+ *****************************************************************************/
+
+//
+
+//#ifndef __BREAK_PLATFORM_WINDOWS_CBUFFER_H__
+//#define __BREAK_PLATFORM_WINDOWS_CBUFFER_H__
+
+struct a2v {
+  float3 position : POSITION;
+  float4 color  : COLOR;
+};
+
+//#endif //__BREAK_PLATFORM_WINDOWS_CBUFFER_H__
